8ef1f3a753f7fa2c5c67c03e00ab213175977584
maximax83 <65817504+maximax83@users.noreply.github.com>
2021-08-16 22:01:20 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const header = document.querySelector('.header');
+const main = document.querySelector('.main');
+const footer = document.querySelector('.footer');
+
+window.addEventListener('resize', function() {
+	// alert('resize' + '\n' + 'window.innerWidth = ' + window.innerWidth);
+
+	header.prepend(footer);
+	header.append(main);
+});
