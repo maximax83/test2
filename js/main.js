1f77981771f45b90280ae209ee17f7da45a3d839
maximax83 <65817504+maximax83@users.noreply.github.com>
2021-08-16 23:20:53 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+alert('start' + '\n' + 'window.innerWidth = ' + window.innerWidth);
+
 const header = document.querySelector('.header');
 const main = document.querySelector('.main');
 const footer = document.querySelector('.footer');
 
 window.addEventListener('resize', function() {
-	alert('resize' + '\n' + 'window.innerWidth = ' + window.innerWidth);
+	// alert('resize' + '\n' + 'window.innerWidth = ' + window.innerWidth);
 
 	header.prepend(footer);
 	header.append(main);
